a9460febd52323fa9b493efcbdf2601ad577f9f6
Matt-Kron <24357592+Matt-Kron@users.noreply.github.com>
2026-09-07 20:55:34 +0200

    bug import

--- Ajouter_porte_pente_g.py
+++ Ajouter_porte_pente_g.py
@@ -15,7 +15,7 @@ def main():
     part = addObjectPartBodyBox(dftStruct, App.ActiveDocument,"Caisson")
     if sel_obj:
         Gui.Selection.addSelection(part)
-        from FreeCAD_BespokeFurniture.PartBetween2Other import run_orchestrator
+        from PartBetween2Other import run_orchestrator
         run_orchestrator()
 
 if __name__ == "__main__":
